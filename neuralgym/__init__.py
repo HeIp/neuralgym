da06741228f7f30fc2f89423066261e4d29e05c7
Carlo Antonio Venditti <c.a.venditti@pm.me>
2024-12-10 11:45:21 +0100

    Add proper .gitignore, small Conv2D fix

--- neuralgym/__init__.py
+++ neuralgym/__init__.py
@@ -1,14 +1,22 @@
 import datetime
-import logging
-import os
 
-from logging.config import dictConfig
-from .utils.logger import colorize
+from logging.config import dictConfig as dictConfig
+from .utils.logger import colorize as colorize
+
+from . import callbacks as callbacks
+from . import ops as ops
+from . import train as train
+from . import models as models
+from . import data as data
+from . import server as server
+
+from .utils.gpus import set_gpus as set_gpus, get_gpus as get_gpus
+from .utils.tf_utils import get_sess as get_sess
+from .utils.config import Config as Config
 
 
-__version__ = '0.0.1'
-__all__ = ['Config', 'get_gpus', 'set_gpus', 'date_uid', 'unset_logger',
-           'get_sess']
+__version__ = "0.0.1"
+__all__ = ["Config", "get_gpus", "set_gpus", "date_uid", "unset_logger", "get_sess"]
 
 
 def date_uid():
@@ -18,19 +26,10 @@ def date_uid():
         str: Return uid string, e.g. '20171122171307111552'.
 
     """
-    return str(datetime.datetime.now()).replace(
-        '-', '').replace(
-            ' ', '').replace(
-                ':', '').replace('.', '')
-
-
-from . import callbacks
-from . import ops
-from . import train
-from . import models
-from . import data
-from . import server
-
-from .utils.gpus import set_gpus, get_gpus
-from .utils.tf_utils import get_sess
-from .utils.config import Config
+    return (
+        str(datetime.datetime.now())
+        .replace("-", "")
+        .replace(" ", "")
+        .replace(":", "")
+        .replace(".", "")
+    )
